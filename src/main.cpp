08184bfd516bcea667fb8e5b3757a91aa1542658
Trevor Robbins <trevorprobbins@gmail.com>
2026-06-26 18:46:59 -0700

    Added circle vertex function

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,54 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
+#include <math.h>
+#include <cmath.h>
+#define M_PI 3.14159265358979323846
 
 struct Vertex 
 {
     float x, y, z;
-}
+};
 
-Vertex* generateCircleVertex(float cx, float cy, float cz, int segments)
+Vertex generateCart(float phi, float theta, float r)
 {
-    
+    Vertex *cart;
+    cart->x = r * sin(phi) * cos(theta);
+    cart->y = r * cos(phi);
+    cart->z = r * sin(phi) * sin(theta);
+    return *cart;
+
+}
+Vertex* generateCircleVertex(float r, int stacks, int sectors, int *outCount)
+{
+    int triCount = stacks * sectors * 2;
+    int vertCount = triCount * 3; // 3 vertices per triangle.
+    Vertex *verts = new Vertex[vertCount];
+    int idx = 0;
+
+    for (int i = 0; i < stacks; i++){
+        float ph1 = M_PI * (i / (float)stacks);
+        float ph2 = M_PI * ((i+ 1) / (float)stacks);
+        for (int j = 0; j < sectors; j++)
+        {
+            float theta1 = ( 2 * M_PI ) * (j / (float)sectors);
+            float theta2 = ( 2 * M_PI ) * ((j+1) / (float)sectors);
+            Vertex A = generateCart(r, theta1, ph1);
+            Vertex B = generateCart(r, theta1, ph2);
+            Vertex C = generateCart(r, theta2, ph1);
+            Vertex D = generateCart(r, theta2, ph2);
+
+            verts[idx++] = A;
+            verts[idx++] = C;
+            verts[idx++] = B;
+
+            verts[idx++] = B;
+            verts[idx++] = C;
+            verts[idx++] = D;
+        }
+    }
+    *outCount = vertCount; 
+    return verts;
 }
 // Callback to handle window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
